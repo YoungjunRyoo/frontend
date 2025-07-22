fa38ea01e629be97ea2537fde7e62681440e3b46
Youngjun Ryoo <youngjunryoo@Youngjuns-MacBook-Air.local>
2025-07-21 22:35:44 -0500

    initial commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,39 +4,18 @@ import "../css/Home.css"
 
 
 function Home(){
-    const [searchQuery, setSearchQuery] = useState("");
-
-    const foods =[
-        {id: 1, name: "SpamEgg", price: 14.99, image:"/images/foods/starter/spamEgg.png"},
-        {id: 2, name: "tteokbokki", price: 23.99, image:"/images/foods/starter/tteokbokki.png"},
-        {id: 3, name: "eggTot", price: 11.99, image:"/images/foods/starter/eggTot.png"},
-        {id: 4, name: "frenchFries", price: 7.99, image:"/images/foods/starter/frenchFries.png"},
-        {id: 5, name: "steamedEgg", price: 11.99, image:"/images/foods/starter/steamedEgg.png"},
-        {id: 6, name: "eggRoll", price: 14.99, image:"/images/foods/starter/eggRoll.png"},
-        {id: 7, name: "ramen", price: 8.99, image:"/images/foods/starter/ramen.png"},
-        {id: 8, name: "edamame", price: 7.99, image:"/images/foods/starter/edamame.png"},
-        {id: 9, name: "soondae", price: 22.99, image:"/images/foods/starter/soondae.png"},
-        {id: 10, name: "haemulPajeon", price: 16.99, image:"/images/foods/starter/haemulPajeon.png"},
-        {id: 11, name: "kimchiJeon", price: 16.99, image:"/images/foods/starter/kimchiJeon.png"},
-        {id: 12, name: "cornCheese", price: 9.99, image:"/images/foods/starter/cornCheese.png"},
-        {id: 13, name: "seaweedSalad", price: 11.99, image:"/images/foods/starter/seaweedSalad.png"},
-
-    ]
-    const handleSearch = (e) =>{
-        e.preventDefault();
-    }
-
+    const base = import.meta.env.BASE_URL;
+    
     return(
         <div className="home">
             <div className="head-image">
-                <img src="images/poster/poster3.png"/>
-                <img src="images/poster/poster4.png"/>
-                <img src="images/poster/poster2.png"/>
-                <img src="images/poster/poster1.png"/>
+                <img src={`${base}images/poster/poster3.png`} alt="poster3" />
+                <img src={`${base}images/poster/poster4.png`} alt="poster4" />
+                <img src={`${base}images/poster/poster2.png`} alt="poster2" />
+                <img src={`${base}images/poster/poster1.png`} alt="poster1" />
             </div>
-    
         </div>
-    );
+  );
 }
 
 export default Home
