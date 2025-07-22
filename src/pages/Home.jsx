4c67468e5971fac1651cc2b02b6bc105758c2a35
Youngjun Ryoo <youngjunryoo@Youngjuns-MacBook-Air.local>
2025-07-22 11:49:44 -0500

    remove / from the img path

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,15 +4,15 @@ import "../css/Home.css"
 
 
 function Home(){
-    const base = import.meta.env.BASE_URL;
+    
     
     return(
         <div className="home">
             <div className="head-image">
-                <img src={`${base}images/poster/poster3.png`} alt="poster3" />
-                <img src={`${base}images/poster/poster4.png`} alt="poster4" />
-                <img src={`${base}images/poster/poster2.png`} alt="poster2" />
-                <img src={`${base}images/poster/poster1.png`} alt="poster1" />
+                <img src={"images/poster/poster3.png"} alt="poster3" />
+                <img src={"images/poster/poster4.png"} alt="poster4" />
+                <img src={"images/poster/poster2.png"} alt="poster2" />
+                <img src={"images/poster/poster1.png"} alt="poster1" />
             </div>
         </div>
   );
